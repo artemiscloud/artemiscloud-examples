63abd11a4bc8558d6eb3ec04175c19383e510e19
Howard Gao <howard.gao@gmail.com>
2023-02-27 23:27:25 +0800

    Update deploy/undeploy operator scripts

--- operator/undeploy_operator.sh
+++ operator/undeploy_operator.sh
@@ -4,19 +4,19 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 source ${BASEDIR}/setup_env.sh
 
-BASE_URL="https://raw.githubusercontent.com/artemiscloud/activemq-artemis-operator/${OPERATOR_VERSION}/deploy/install"
+BASE_URL="https://raw.githubusercontent.com/artemiscloud/activemq-artemis-operator/${OPERATOR_VERSION}/deploy"
 
-${KUBE_CLI} delete -f ${BASE_URL}/010_crd_artemis.yaml
-${KUBE_CLI} delete -f ${BASE_URL}/020_crd_artemis_security.yaml
-${KUBE_CLI} delete -f ${BASE_URL}/030_crd_artemis_address.yaml
-${KUBE_CLI} delete -f ${BASE_URL}/040_crd_artemis_scaledown.yaml
+${KUBE_CLI} delete -f ${BASE_URL}/crds/broker_activemqartemis_crd.yaml
+${KUBE_CLI} delete -f ${BASE_URL}/crds/broker_activemqartemissecurity_crd.yaml
+${KUBE_CLI} delete -f ${BASE_URL}/crds/broker_activemqartemisaddress_crd.yaml
+${KUBE_CLI} delete -f ${BASE_URL}/crds/broker_activemqartemisscaledown_crd.yaml
 
-${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/050_service_account.yaml
-${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/060_namespace_role.yaml
-${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/070_namespace_role_binding.yaml
-${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/080_election_role.yaml
-${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/090_election_role_binding.yaml
-${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/100_operator_config.yaml
-${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/110_operator.yaml
+${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/service_account.yaml
+${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/role.yaml
+${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/role_binding.yaml
+${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/election_role.yaml
+${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/election_role_binding.yaml
+${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/operator_config.yaml
+${KUBE_CLI} delete -n ${OPR_NAMESPACE} -f ${BASE_URL}/operator.yaml
 
 echo "Done."
